99e78c70092855be159153522812de7502dea2d9
Mahad-dev <mahadkhurshid4052@gmail.com>
2024-02-04 14:16:14 +0500

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,12 @@
-import Sidebar  from './sidebar'
- 
+import Sidebar from './sidebar'
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Layout({ children }) {
+
   return (
-    <div className='grid grid-cols-[240px_1fr]'>
-        <Sidebar />
+    <div className={`grid grid-cols-[240px_1fr] ${inter.className}`}>
+      <Sidebar />
       <main>{children}</main>
     </div>
   )
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,46 +1,62 @@
-import { DealFlow, Finance, Minus, Plus } from "../ui-icons"
+import Link from "next/link"
+import { Contacts, DealFlow, Finance, Minus, Plugins, Plus } from "../ui-icons"
 
+
+
+const navLinks = [
+    {
+        id: 1,
+        name: "Dealflow",
+        icon: <DealFlow />,
+        href: ""
+    },
+    {
+        id: 2,
+        name: "Invoice",
+        icon: <Plus />,
+        href: ""
+    },
+    {
+        id: 3,
+        name: "Pay",
+        icon: <Minus />,
+        href: ""
+
+    },
+    {
+        id: 4,
+        name: "Financing",
+        icon: <Finance />,
+        href: ""
+
+    },
+    {
+        id: 5,
+        name: "Contacts",
+        icon: <Contacts />,
+        href: ""
+    },
+    {
+        id: 6,
+        name: "Plugins",
+        icon: <Plugins />,
+        href: ""
+    },
+]
 export default function Sidebar() {
 
-    const navLinks = [
-        {
-            id: 1,
-            name: "Dealflow",
-            icon: <DealFlow />
-        },
-        {
-            id: 2,
-            name: "Invoice",
-            icon: <Plus />
-        },
-        {
-            id: 3,
-            name: "Pay",
-            icon: <Minus />
-        },
-        {
-            id: 4,
-            name: "Financing",
-            icon: <Finance />
-        },
-        {
-            id: 5,
-            name: "Contacts",
-            icon: <Finance />
-        },
-        {
-            id: 6,
-            name: "Plugins",
-            icon: <Finance />
-        },
-    ]
+
     return (
         <div className="p-[20px] border-r-[1px] border-[#647084]">
-            <ul>
+            <ul className="flex flex-col gap-8">
                 {navLinks.map((link => (
-                    <li key={link.id} className="flex items-center gap-3 text-[#120713] text-[13px]">
-                        {link.icon}
-                        {link.name}
+                    <li key={link.id} className="text-[#120713] text-[13px]">
+                        <Link href={link.href}
+                        className="flex items-center gap-3"
+                        >
+                            {link.icon}
+                            {link.name}
+                        </Link>
                     </li>
                 )))
                 }
